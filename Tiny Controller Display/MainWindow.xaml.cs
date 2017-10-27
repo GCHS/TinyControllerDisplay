b43497bc279c6a096d8fdb5c5213fba4b6722b64
Giancarlo Saraceni <ghsaraceni@yahoo.com>
2017-10-26 19:11:02 -0700

    Update to .NET 4.7, initial DPI detection

--- Tiny Controller Display/MainWindow.xaml.cs
+++ Tiny Controller Display/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Input;
 using SharpDX.XInput;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Interop;
 
 namespace Tiny_Controller_Display {
 	/// <summary>
@@ -15,9 +16,33 @@ namespace Tiny_Controller_Display {
 	/// </summary>
 	public partial class MainWindow : Window {
 		private ControllerDisplayUpdater displayUpdater;
-
+		private double dpiX, dpiY;
 		public MainWindow() {
 			InitializeComponent();
+			{
+				var source = PresentationSource.FromVisual(controllerDisplay);
+				Matrix scaleMatrix;
+				if(source != null) {
+					scaleMatrix = source.CompositionTarget.TransformToDevice;
+				} else {
+					using(var src = new HwndSource(new HwndSourceParameters())) {
+						scaleMatrix = src.CompositionTarget.TransformToDevice;
+					}
+				}
+				dpiX = scaleMatrix.M11;
+				dpiY = scaleMatrix.M22;
+			}
+			
+			//foreach(Image i in new Image[18] {
+			//		leftBumper, rightBumper, faceplate, bottomButton,
+			//		dPad, leftButton, leftStickwellPressed, leftSticktop,
+			//		leftSticktopPressed, rightButton, rightStickwellPressed,
+			//		rightSticktop, rightSticktopPressed, selectButton,
+			//		startButton, topButton, leftTrigger, rightTrigger }) {
+			//	i.Width /= dpiX;
+			//	i.Height /= dpiY;
+			//}
+
 			displayUpdater = new ControllerDisplayUpdater(
 				new Dictionary<GamepadButtonFlags, Image[]>(){
 					{GamepadButtonFlags.A, new Image[1]{bottomButton}},
