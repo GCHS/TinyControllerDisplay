7af38d58c8a6b192e4265fd3c330dd9bcf599c16
Giancarlo Saraceni <ghsaraceni@gmail.com>
2018-09-10 22:28:46 -0700

    Multiple-controller support. Right-click menu offers togglable menu options for each controller. Each has its own window.

--- Tiny Controller Display/ControllerDisplayUpdater.cs
+++ Tiny Controller Display/ControllerDisplayUpdater.cs
@@ -16,8 +16,8 @@ namespace Tiny_Controller_Display {
 	}
 
 	class ControllerDisplayUpdater {
-		private Controller controller1=new Controller(UserIndex.One);
-		private State player1 = new State();
+		private Controller controller;
+		private State player = new State();
 		private readonly Task updateTask;
 		private Thickness noMargin = new Thickness(0), leftBumperPressed = new Thickness(1, 1, 0, 0), rightBumperPressed = new Thickness(-2, 1, 0, 0);//negative margins need to be doubled to display properly. 
 
@@ -27,10 +27,12 @@ namespace Tiny_Controller_Display {
 		private RectangleGeometry leftArcClip, rightArcClip;
 
 		public ControllerDisplayUpdater(
+			UserIndex userIndex,
 			Dictionary<GamepadButtonFlags, Image[]> buttonsToImages,
 			Image dPad, Image leftBumper, Image rightBumper,
 			Stick leftStick, Stick rightStick,
 			RectangleGeometry leftArcClip, RectangleGeometry rightArcClip) {
+			controller = new Controller(userIndex);
 			this.buttonsToImages = buttonsToImages;
 			this.dPad = dPad;
 			this.leftBumper = leftBumper;
@@ -47,8 +49,8 @@ namespace Tiny_Controller_Display {
 		}
 
 		Thickness GetDPadMargin() {
-			double topMargin = Convert.ToDouble((player1.Gamepad.Buttons & GamepadButtonFlags.DPadDown) != 0) - Convert.ToDouble((player1.Gamepad.Buttons & GamepadButtonFlags.DPadUp) != 0) * 2.0;
-			double leftMargin = Convert.ToDouble((player1.Gamepad.Buttons & GamepadButtonFlags.DPadRight) != 0) - Convert.ToDouble((player1.Gamepad.Buttons & GamepadButtonFlags.DPadLeft) != 0) * 2.0;
+			double topMargin = Convert.ToDouble((player.Gamepad.Buttons & GamepadButtonFlags.DPadDown) != 0) - Convert.ToDouble((player.Gamepad.Buttons & GamepadButtonFlags.DPadUp) != 0) * 2.0;
+			double leftMargin = Convert.ToDouble((player.Gamepad.Buttons & GamepadButtonFlags.DPadRight) != 0) - Convert.ToDouble((player.Gamepad.Buttons & GamepadButtonFlags.DPadLeft) != 0) * 2.0;
 			return new Thickness(leftMargin, topMargin, 0, 0);
 		}
 
@@ -57,22 +59,22 @@ namespace Tiny_Controller_Display {
 		}
 
 		private void Update() {
-			controller1.GetState(out player1);
+			controller.GetState(out player);
 			foreach(KeyValuePair<GamepadButtonFlags,Image[]> buttonToImages in buttonsToImages) {
 				foreach(Image i in buttonToImages.Value) {
-					if((player1.Gamepad.Buttons & buttonToImages.Key) != 0) {
+					if((player.Gamepad.Buttons & buttonToImages.Key) != 0) {
 						i.Visibility = Visibility.Visible;
 					} else {
 						i.Visibility = Visibility.Hidden;
 					}
 				}
 			}
-			leftStick.unpressedSticktop.Margin = leftStick.pressedSticktop.Margin = StickValueToMargin(player1.Gamepad.LeftThumbX, player1.Gamepad.LeftThumbY);
-			rightStick.unpressedSticktop.Margin = rightStick.pressedSticktop.Margin = StickValueToMargin(player1.Gamepad.RightThumbX, player1.Gamepad.RightThumbY);
-			leftBumper.Margin = ((player1.Gamepad.Buttons & GamepadButtonFlags.LeftShoulder) != 0) ? leftBumperPressed : noMargin;
-			rightBumper.Margin = ((player1.Gamepad.Buttons & GamepadButtonFlags.RightShoulder) != 0) ? rightBumperPressed : noMargin;
-			leftArcClip.Rect = new Rect(0, TriggerToArcClipY(player1.Gamepad.LeftTrigger), 57, 37);
-			rightArcClip.Rect = new Rect(0, TriggerToArcClipY(player1.Gamepad.RightTrigger), 57, 37);
+			leftStick.unpressedSticktop.Margin = leftStick.pressedSticktop.Margin = StickValueToMargin(player.Gamepad.LeftThumbX, player.Gamepad.LeftThumbY);
+			rightStick.unpressedSticktop.Margin = rightStick.pressedSticktop.Margin = StickValueToMargin(player.Gamepad.RightThumbX, player.Gamepad.RightThumbY);
+			leftBumper.Margin = ((player.Gamepad.Buttons & GamepadButtonFlags.LeftShoulder) != 0) ? leftBumperPressed : noMargin;
+			rightBumper.Margin = ((player.Gamepad.Buttons & GamepadButtonFlags.RightShoulder) != 0) ? rightBumperPressed : noMargin;
+			leftArcClip.Rect = new Rect(0, TriggerToArcClipY(player.Gamepad.LeftTrigger), 57, 37);
+			rightArcClip.Rect = new Rect(0, TriggerToArcClipY(player.Gamepad.RightTrigger), 57, 37);
 			dPad.Margin = GetDPadMargin();
 		}
 
--- Tiny Controller Display/MainWindow.xaml.cs
+++ Tiny Controller Display/MainWindow.xaml.cs
@@ -13,12 +13,24 @@ namespace Tiny_Controller_Display {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
+	public partial class ControllerDisplay : Window {
 		private ControllerDisplayUpdater displayUpdater;
 
-		public MainWindow() {
+		private static Dictionary<UserIndex, ControllerDisplay> userIndexToDisplay = new Dictionary<UserIndex, ControllerDisplay>();
+
+		public ControllerDisplay() : this(UserIndex.One) { }
+
+		public ControllerDisplay(UserIndex userIndex) {
 			InitializeComponent();
+			userIndexToDisplay[userIndex] = this;
+
+			if(userIndexToDisplay.ContainsKey(UserIndex.One))   { Controller1Toggle.IsChecked   = true; }
+			if(userIndexToDisplay.ContainsKey(UserIndex.Two))   { Controller2Toggle.IsChecked   = true; }
+			if(userIndexToDisplay.ContainsKey(UserIndex.Three)) { Controller3Toggle.IsChecked   = true; }
+			if(userIndexToDisplay.ContainsKey(UserIndex.Four))  { Controller4Toggle.IsChecked   = true; }
+
 			displayUpdater = new ControllerDisplayUpdater(
+				userIndex,
 				new Dictionary<GamepadButtonFlags, Image[]>(){
 					{GamepadButtonFlags.A, new Image[1]{bottomButton}},
 					{GamepadButtonFlags.B, new Image[1]{rightButton}},
@@ -31,13 +43,33 @@ namespace Tiny_Controller_Display {
 				},
 				dPad, leftBumper, rightBumper,
 				new Stick(leftSticktop, leftSticktopPressed), new Stick(rightSticktop, rightSticktopPressed),
-				(leftTrigger.Clip as RectangleGeometry), (rightTrigger.Clip as RectangleGeometry)
+				leftTrigger.Clip as RectangleGeometry, rightTrigger.Clip as RectangleGeometry
 			);
 		}
+
 		private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
 			if(e.ChangedButton == MouseButton.Left)
 				DragMove();
 		}
 
+		private void ToggleDisplay(UserIndex userIndex) {
+			lock(userIndexToDisplay) {
+				if(!userIndexToDisplay.ContainsKey(userIndex)) {
+					(userIndexToDisplay[userIndex] = new ControllerDisplay(userIndex) {
+						Top = Top,
+						Left = Left + Math.Floor(Width * 1.25)
+					}).Show();
+				}else{//toggling off
+					userIndexToDisplay[userIndex].Close();
+					userIndexToDisplay.Remove(userIndex);
+				}
+			}
+		}
+
+
+		private void Controller1Toggle_Click(object sender, RoutedEventArgs e) => ToggleDisplay(UserIndex.One);
+		private void Controller2Toggle_Click(object sender, RoutedEventArgs e) => ToggleDisplay(UserIndex.Two);
+		private void Controller3Toggle_Click(object sender, RoutedEventArgs e) => ToggleDisplay(UserIndex.Three);
+		private void Controller4Toggle_Click(object sender, RoutedEventArgs e) => ToggleDisplay(UserIndex.Four);
 	}
 }
